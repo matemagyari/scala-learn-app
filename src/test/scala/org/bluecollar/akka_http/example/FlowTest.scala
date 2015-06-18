4c5a7d3399dab885c281fb46f07c75ad25d93170
mate.magyari <mate.magyari@gmail.com>
2015-06-18 10:43:26 +0100

    change test

--- src/test/scala/org/bluecollar/akka_http/example/FlowTest.scala
+++ src/test/scala/org/bluecollar/akka_http/example/FlowTest.scala
@@ -18,10 +18,9 @@ class FlowTest extends FunSuite {
       Source(() => Iterator.continually(ThreadLocalRandom.current().nextInt(100000)))
         .filter(_ % 2 == 0)
 
-    val sumSink = Sink.fold(0)(_+_)
+    //val sumSink = Sink.fold(0)(_+_)
 
-    val materialized = FlowGraph.closed(sumSink)
-
+    //val materialized = FlowGraph.closed(sumSink)
 
     //primeSource.runWith(Sink.foreach(println))
 
